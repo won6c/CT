c809f011827c9427b7f70a164812fbf25ad3e54e
won6c <superken1227@naver.com>
2025-05-12 14:26:26 +0900

    endl때문에 계속 틀림

--- 17425/17425.cpp
+++ 17425/17425.cpp
@@ -1,21 +1,41 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-int sumation(int n){
-    int sum=0;
-    for(int i = 1;i<=n;i++){
-        sum+=(n/i)*i;
+long long arr[1000001];
+long long sum[1000001];
+
+void sumation(){
+    for(long long i = 1;i<=1000000;i++){
+        if(i==1){
+            sum[i] = arr[i];
+        }else{
+            sum[i] = sum[i-1]+arr[i];
+        }
     }
-    return sum;
+}
+
+void init(){
+    for(long long i = 1;i<=1000000;i++){
+        for(long long ii = 1;i*ii<=1000000;ii++){
+            arr[i*ii]+=i;
+        }
+    }
 }
 
 int main(){
-    int T, N;
+    int T;
+    long long N;
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
     cin>>T;
+
+    init();
+    sumation();
+
     for(int i = 0;i<T;i++){
         cin>>N;
-        cout<<sumation(N)<<"\n";
+        cout<<sum[N]<<"\n";
     }
 
     return 0;
